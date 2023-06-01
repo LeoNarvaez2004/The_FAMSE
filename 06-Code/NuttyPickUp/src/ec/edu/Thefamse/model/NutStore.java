81a525045e79706a08e1631bdf5dd6074a078052
Anthony <anthonyalainmorales02@gmail.com>
2023-06-01 04:15:16 -0500

    SystemCode v0.5.0

--- 06-Code/NuttyPickUp/src/ec/edu/Thefamse/model/NutStore.java
+++ 06-Code/NuttyPickUp/src/ec/edu/Thefamse/model/NutStore.java
@@ -1,6 +1,7 @@
 package ec.edu.Thefamse.model;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,11 +13,12 @@ public class NutStore {
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
     int option;
+    boolean salir = false;
     NutProduct product = null;
     Reservation reservation = null;
     List<NutCustomer> customers = new ArrayList<>();
 
-        do {
+        while(!salir){
             System.out.println("Menu de opciones: ");
             System.out.println("1. Crear nuevo usuario.");
             System.out.println("2. Agregar nuevo producto.");
@@ -25,60 +27,69 @@ public class NutStore {
             System.out.println("5. Estado de compra.");
             System.out.println("6. Reporte.");
             System.out.println("7. Salir.");
+            
+            try{
+                
+            
             System.out.println("Ingrese la opcion que desea visualizar: ");
             option = scanner.nextInt();
 
-            switch (option) {
-                case 1:
-                    System.out.println("--Usted a ingresado en creacion de usuario--");
-                    NutCustomer customer = NutCustomer.createAccount();
-                    System.out.println("Cuenta creada.");
-                    break;
-                case 2:
-                    product = NutProduct.addProduct();
-                    System.out.println("Producto agregado.");
-                    break;
-                case 3:
-                    if (product == null) {
-                        System.out.println("No se ha seleccionado ningun producto.");
-                    } else {
-                        if (reservation == null) {
-                            reservation = new Reservation(product);
-                        }
-
-                        if (reservation.isPurchased()) {
-                            System.out.println("Ya realizo la compra.");
+                switch (option) {
+                    case 1:
+                        System.out.println("--Usted a ingresado en creacion de usuario--");
+                        NutCustomer customer = NutCustomer.createAccount();
+                        System.out.println("Cuenta creada.");
+                        break;
+                    case 2:
+                        product = NutProduct.addProduct();
+                        System.out.println("Producto agregado.");
+                        break;
+                    case 3:
+                        if (product == null) {
+                            System.out.println("No se ha seleccionado ningun producto.");
                         } else {
-                            reservation.purchase();
-                        }
-                    }
-                    break;
-                case 4:
-                       if (reservation == null) {
-                         System.out.println("No se ha realizado ninguna compra.");
-                         } else {
-                         PurchaseModifier purchaseModifier = new PurchaseModifier(reservation);
-                            purchaseModifier.modifyPurchase();
-                         }
-                break;
-                case 5:
-                       if (reservation == null) {
-                           System.out.println("No se ha realizado ninguna compra.");
-                           } else {
-                           State state = new State(reservation);
-                           state.viewState();
-                           }
-                    break;
-                case 6:
+                            if (reservation == null) {
+                                reservation = new Reservation(product);
+                            }
 
-                    break;
-                case 7:
-                    break;
-                default:
-                    System.out.println("Opción incorrecta, ingrese de nuevo: ");
-                    break;
+                            if (reservation.isPurchased()) {
+                                System.out.println("Ya realizo la compra.");
+                            } else {
+                                reservation.purchase();
+                            }
+                        }
+                        break;
+                    case 4:
+                        if (reservation == null) {
+                            System.out.println("No se ha realizado ninguna compra.");
+                        } else {
+                            PurchaseModifier purchaseModifier = new PurchaseModifier(reservation);
+                            purchaseModifier.modifyPurchase();
+                        }
+                        break;
+                    case 5:
+                        if (reservation == null) {
+                            System.out.println("No se ha realizado ninguna compra.");
+                        } else {
+                            State state = new State(reservation);
+                            state.viewState();
+                        }
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        System.out.println("Opción incorrecta, ingrese de nuevo: ");
+                        break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Solo puede ingresar numeros.");
+                scanner.next();
             }
-        } while (option != 7);
+        
+        }
 
     }
 }
