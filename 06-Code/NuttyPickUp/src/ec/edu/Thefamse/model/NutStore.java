846234c0963530e3e22a4c883ab4ce5e96d7d33d
LeoNarvaez2503 <leonardo_8.8@live.com>
2023-06-04 19:12:19 -0500

    Update

--- 06-Code/NuttyPickUp/src/ec/edu/Thefamse/model/NutStore.java
+++ 06-Code/NuttyPickUp/src/ec/edu/Thefamse/model/NutStore.java
@@ -7,37 +7,45 @@ import java.util.Scanner;
 
 /**
  *
- * @author THE FANSE.
+ * author THE FANSE.
  */
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class NutStore {
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
-    int option;
-    boolean salir = false;
-    NutProduct product = null;
-    Reservation reservation = null;
-    List<NutCustomer> customers = new ArrayList<>();
+        int option;
+        boolean salir = false;
+        NutProduct product = null;
+        Reservation reservation = null;
+        List<NutCustomer> customers = new ArrayList<>();
 
-        while(!salir){
+        while (!salir) {
             System.out.println("Menu de opciones: ");
             System.out.println("1. Crear nuevo usuario.");
             System.out.println("2. Agregar nuevo producto.");
             System.out.println("3. Comprar producto.");
             System.out.println("4. Editar compra.");
             System.out.println("5. Estado de compra.");
-            System.out.println("6. Reporte.");
+            System.out.println("6. Generar archivo JSON de reportes.");
             System.out.println("7. Salir.");
-            
-            try{
-                
-            
-            System.out.println("Ingrese la opcion que desea visualizar: ");
-            option = scanner.nextInt();
+
+            try {
+                System.out.println("Ingrese la opcion que desea visualizar: ");
+                option = scanner.nextInt();
 
                 switch (option) {
                     case 1:
-                        System.out.println("--Usted a ingresado en creacion de usuario--");
+                        System.out.println("--Usted ha ingresado en creacion de usuario--");
                         NutCustomer customer = NutCustomer.createAccount();
+                        customers.add(customer); // Agregar el cliente a la lista de clientes
                         System.out.println("Cuenta creada.");
                         break;
                     case 2:
@@ -46,7 +54,7 @@ public class NutStore {
                         break;
                     case 3:
                         if (product == null) {
-                            System.out.println("No se ha seleccionado ningun producto.");
+                            System.out.println("No se ha seleccionado ningún producto.");
                         } else {
                             if (reservation == null) {
                                 reservation = new Reservation(product);
@@ -76,20 +84,22 @@ public class NutStore {
                         }
                         break;
                     case 6:
-
+                         System.out.println("--Generar reporte--");
+                        Report.generateReportJSON(customers);
+                        System.out.println("Reporte generado.");
                         break;
                     case 7:
+                        salir = true; // Actualizar la variable salir para salir del bucle
+                        System.out.println("Gracias, buen dia.");
                         break;
                     default:
                         System.out.println("Opción incorrecta, ingrese de nuevo: ");
                         break;
                 }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Solo puede ingresar numeros.");
                 scanner.next();
             }
-        
         }
-
     }
 }
