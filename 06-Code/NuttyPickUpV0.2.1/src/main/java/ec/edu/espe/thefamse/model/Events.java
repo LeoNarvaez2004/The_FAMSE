fe4d57d55350b03bd926b07197b1abd4d758acbf
Anthony <anthonyalainmorales02@gmail.com>
2023-08-12 02:48:30 -0500

    Update Code version 0.2.1

---
+++ 06-Code/NuttyPickUpV0.2.1/src/main/java/ec/edu/espe/thefamse/model/Events.java
@@ -0,0 +1,35 @@
+package ec.edu.espe.thefamse.model;
+
+import java.awt.event.KeyEvent;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Anthony Morales, The_FAMSE
+ */
+public class Events {
+    public void textKeyPress(KeyEvent evt) {
+        char car = evt.getKeyChar();
+        if ((car < 'a' || car > 'z') && (car < 'A' || car > 'Z')
+                && (car != (char) KeyEvent.VK_BACK_SPACE) && (car != (char) KeyEvent.VK_SPACE)) {
+            evt.consume();
+        }
+    }
+
+    public void numberKeyPress(KeyEvent evt) {
+        char car = evt.getKeyChar();
+        if ((car < '0' || car > '9') && (car != (char) KeyEvent.VK_BACK_SPACE)) {
+            evt.consume();
+        }
+    }
+
+    public void numberDecimalKeyPress(KeyEvent evt, JTextField textField) {
+
+        char car = evt.getKeyChar();
+        if ((car < '0' || car > '9') && textField.getText().contains(".") && (car != (char) KeyEvent.VK_BACK_SPACE)) {
+            evt.consume();
+        } else if ((car < '0' || car > '9') && (car != '.') && (car != (char) KeyEvent.VK_BACK_SPACE)) {
+            evt.consume();
+        }
+    }
+}
