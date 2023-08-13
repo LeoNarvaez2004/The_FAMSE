e4eea28c5840c91790494dc5e2f67bc32e723fc2
Davidmoran9 <davidleonel0408@gmail.com>
2023-08-13 18:53:05 -0500

    Update

---
+++ 06-Code/NuttyPickUp v0.5/src/ec/edu/Thefamse/model/NutCustomer.java
@@ -0,0 +1,20 @@
+package ec.edu.Thefamse.model;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author Team 5, "THE FAMSE"
+ */
+public class InvoiceGenerator {
+    public static void generateInvoice(NutProduct product, Reservation reservation, NutCustomer customer) {
+        System.out.println("Generando factura...");
+        System.out.println("------------------------------");
+        System.out.println("Detalles de la factura:");
+        System.out.println("Cliente: " + customer.getName());
+        System.out.println("Producto: " + product.getName());
+        System.out.println("Precio: $" + product.getPrice());
+        System.out.println("Fecha de compra: " + LocalDate.now());
+        System.out.println("------------------------------");
+        System.out.println("Factura generada exitosamente.");
+    }
+}
